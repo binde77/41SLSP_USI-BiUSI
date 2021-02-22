7bdd10b518e0bd76bf38d22504b9af9fc9f0c050
Silvio Bindella <silvio.bindella@usi.ch>
2021-02-22 14:48:44 +0100

    Updated ILL costs pages & new JS script for handling policy 65 at MEAA

--- js/custom.js
+++ js/custom.js
@@ -5,17 +5,25 @@
     // var app = angular.module('viewCustom', ['angularLoad']);
 
 
-    // COMMENTATO IN ATTESA DI TARIFFE USI
-    // app.controller('CourierInfoController', [function () {
-    //     var vm = this;
+    // TARIFFE USI
+    app.controller('CourierInfoController', [function () {
+        var vm = this;
 
-    //     vm.linkBase = "https:\/\/slsp.ch\/fees";
+        // vm.linkBase = "https:\/\/slsp.ch\/fees";
+        vm.getParms = "?vid=41SLSP_USI:BiUSI";
+        vm.customLinkBase = "\/discovery\/static-file\/costs";
 
-
-    // }]);
+    }]);
 
     // ELIMINATO, VIENE EREDITATO DA CENTRAL CUSTOMIZATION PACKAGE
     // App.component“prmRequestAfter”
+
+
+    app.component('prmRequestAfter', {
+        bindings: { parentCtrl: '<' },
+        controller: 'CourierInfoController',
+        template: '<div layout="row" class="courier-info bar alert-bar layout-align-center-center layout-row" layout-align="center center"><span class="md-subhead"><a href="{{ $ctrl.customLinkBase }}{{ $ctrl.getParms }}" target="_blank"><span translate="customized.fulldisplay.fees"></span></a></span></div>'
+    });
 
 
 
@@ -31,12 +39,13 @@
         vm.biblinkStyle = getStyle;
 
         function getLibrary() {
-            if (vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Biblioteca dell\'Accademia di architettura' || vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Academy of architecture Library') {
-                return 'BIBLIOTECA CHIUSA / LIBRARY CLOSED 03.12.2020-31.01.2021';
-            }
-            else {
-                return vm.parentCtrl.currLoc.location.librarycodeTranslation;
-            }
+            // if (vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Biblioteca dell\'Accademia di architettura' || vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Academy of architecture Library') {
+            //     return 'BIBLIOTECA CHIUSA / LIBRARY CLOSED 03.12.2020-31.01.2021';
+            // }
+            // else {
+            //     return vm.parentCtrl.currLoc.location.librarycodeTranslation;
+            // }
+            return vm.parentCtrl.currLoc.location.librarycodeTranslation;
         }
         function getLink() {
             if (vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Biblioteca dell\'Accademia di architettura' || vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Academy of architecture Library') {
@@ -50,12 +59,13 @@
             }
         }
         function getStyle() {
-            if (vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Biblioteca dell\'Accademia di architettura' || vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Academy of architecture Library') {
-                return 'color: red;';
-            }
-            else {
-                return '';
-            }
+            // if (vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Biblioteca dell\'Accademia di architettura' || vm.parentCtrl.currLoc.location.librarycodeTranslation == 'USI-Academy of architecture Library') {
+            //     return 'color: red;';
+            // }
+            // else {
+            //     return '';
+            // }
+            return '';
         }
 
     }]);
@@ -64,6 +74,38 @@
         bindings: { parentCtrl: '<' },
         controller: 'LibInfoController',
         template: '<div layout="row" class="LibInfo" layout-align="start center"><span class="md-subhead"><a ng-href="{{ $ctrl.biblinkBase() }}" style="{{ $ctrl.biblinkStyle() }}" target="_blank"><img width="35px" ng-src="/discovery/custom/41SLSP_NETWORK-CENTRAL_PACKAGE/img/information.png" />{{ $ctrl.getLibrary() }}</a></span></div>'
+    });
+
+
+
+
+    // Insert customized text per items from MEAA and policy 65 - Same day loan
+    app.controller('ItemCommentComponentController', [function () {
+        var vm = this;
+
+        vm.isMeaa = isMeaa;
+        vm.isSameDay = isSameDay;
+
+        function isMeaa() {
+            if (vm.parentCtrl.loc.location.libraryCode == 'MEAA') {
+                return true;
+            }
+            return false;
+        }
+
+        function isSameDay() {
+            var policy = vm.parentCtrl.loc.items[0].itemFields[1];
+            if (policy.lastIndexOf('Same') > -1) {
+                return true;
+            }
+            return false;
+        }
+    }]);
+
+    app.component('prmLocationItemAfter', {
+        bindings: {parentCtrl: '<'},
+        controller: 'ItemCommentComponentController',
+        template: '<span style="padding: 10px; font-size: 0.9em; color: red; display: flex; max-width: 200px;" ng-if="$ctrl.isMeaa() && $ctrl.isSameDay()"><span translate="customized.arc.sameday"></span></span>'
     });
 
 
