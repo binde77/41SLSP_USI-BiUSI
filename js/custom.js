95e3f3b0879487e704c0dd4b1fb61a71e116e121
Silvio Bindella <silvio.bindella@usi.ch>
2021-07-15 09:53:19 +0200

    Updated personal user account management

--- js/custom.js
+++ js/custom.js
@@ -185,58 +185,57 @@
 
     //------------------------------ edit personal details  ---------------------------
 
-    app.controller('EditPersonalDetailsController', [function () {
+    app.controller('EditPersonalDetailsController', ['$scope', function ($scope) {
 
-        this.detailsBaseEdu = "https:\/\/eduid.ch";
-        this.detailsBaseReg = "https:\/\/registration.slsp.ch\/library-card\/";
-        this.exclude = ['STAFF', '11', '12', '13', '14', '15', '16', '91', '92', '99'];
+        let lang = 'en';
+        let sms = $scope.$root.$$childHead.$ctrl.userSessionManagerService;
+        if (sms) {
+            lang = sms.getInterfaceLanguage();
+        }
+        this.detailsBaseEdu = "https:\/\/eduid.ch\/web\/change-account-data\/2\/?lang=" + lang;
+        this.detailsBaseReg = "https:\/\/registration.slsp.ch\/library-card\/?lang=" + lang;
         this.grpA = ['11', '91', '92'];
         this.grpB = ['12', '13', '14', '15', '16'];
+        this.noShow = ['STAFF', '99'];
+        this.showButtons = this.showA = this.showB = false;
 
-        this.getPatronGrp = function() {
+        this.$doCheck = function () {
             if (this.parentCtrl.personalInfoService.personalInfo !== undefined) {
                 let patron = this.parentCtrl.personalInfoService.personalInfo.patronstatus[0].registration[0].institution[0].patronstatuscode;
-                if (!this.exclude.includes(patron)) {
-                    return true;
+                if (this.grpA.includes(patron)) {
+                    this.showA = true;
                 }
-                else {
-                    return false;
+                else if (this.grpB.includes(patron)) {
+                    this.showB = true;
                 }
-            }
-            return false;
-        }
-
-        this.grpLabelA = function() {
-            if (this.parentCtrl.personalInfoService.personalInfo !== undefined) {
-                let labelA = this.parentCtrl.personalInfoService.personalInfo.patronstatus[0].registration[0].institution[0].patronstatuscode;
-                if (this.grpA.includes(labelA)) {
-                    return true;
-                }
-                else {
-                    return false;
+                else if (!this.noShow.includes(patron)) {
+                    this.showButtons = true;
                 }
             }
-            return false;
         }
-
-
-        this.grpLabelB = function() {
-            if (this.parentCtrl.personalInfoService.personalInfo !== undefined) {
-                let labelB = this.parentCtrl.personalInfoService.personalInfo.patronstatus[0].registration[0].institution[0].patronstatuscode;
-                if (this.grpB.includes(labelB)) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            return false;
-        }
-    }])
+    }]);
     app.component('prmPersonalInfoAfter', {
-        bindings: { parentCtrl: '<' },
-        controller: 'EditPersonalDetailsController',
-        template: '<md-card ng-if="$ctrl.grpLabelA()" translate="customized.libraries.details" flex="100" class="bar alert-bar courier-info"></md-card><md-card ng-if="$ctrl.grpLabelB()" translate="customized.slsp.details" flex="100" class="bar alert-bar courier-info"></md-card><div layout="column"> <md-button ng-if="$ctrl.getPatronGrp()" href="{{ $ctrl.detailsBaseEdu }}" target="_blank" layout="row" class="courier-info bar alert-bar layout-align-left-center layout-row" layout-align="left center"><span class="md-subhead"><prm-icon  class="rotate-20 margin-right-small" icon-type="svg" svg-icon-set="primo-ui" icon-definition="pencil"></prm-icon><span  class="md-subhead" translate="customized.personal.details"></span></md-button><br /><md-button ng-if="$ctrl.getPatronGrp()" href="{{ $ctrl.detailsBaseReg }}" target="_blank" layout="row" class="courier-info bar alert-bar layout-align-left-center layout-row" layout-align="left center"><prm-icon class="rotate-20 margin-right-small" icon-type="svg" svg-icon-set="primo-ui" icon-definition="account-card-details"></prm-icon><span  class="md-subhead" translate="customized.libcard.number"></span></md-button ></div>'
+    bindings: { parentCtrl: '<' },
+    controller: 'EditPersonalDetailsController',
+    template: '<md-card ng-show="$ctrl.showA || $ctrl.showB">\
+                    <md-card-content>\
+                        <p>\
+                            <span ng-show="$ctrl.showA" translate="customized.libraries.details"></span>\
+                            <span ng-show="$ctrl.showB" translate="customized.slsp.details"></span>\
+                        </p>\
+                    </md-card-content>\
+                </md-card>\
+                <div ng-show="$ctrl.showButtons">\
+                    <md-button class="md-button-confirm button-link" href="{{ $ctrl.detailsBaseEdu }}" target="_blank">\
+                        <prm-icon  class="rotate-20 margin-right-small" icon-type="svg" svg-icon-set="primo-ui" icon-definition="pencil"></prm-icon>\
+                        <span translate="customized.personal.details"></span>\
+                    </md-button>\
+                    <br>\
+                    <md-button class="md-button-confirm button-link" href="{{ $ctrl.detailsBaseReg }}" target="_blank">\
+                        <prm-icon class="rotate-20 margin-right-small" icon-type="svg" svg-icon-set="primo-ui" icon-definition="account-card-details"></prm-icon>\
+                        <span translate="customized.libcard.number"></span>\
+                    </md-button>\
+                </div>'
     });
 
 
